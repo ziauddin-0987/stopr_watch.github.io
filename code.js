3e6ea8ac8099aa93461e9aedad9b1dbb9f996140
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-28 23:51:26 +0500

    this code my js in a map with ayy

---
+++ code.js
@@ -0,0 +1,11 @@
+let codding = ["zia", "nabil", "javad", "kareem", "farman", "irfan", "kamran"];
+
+let code = codding.filter((items) => {
+  return items;
+});
+console.log(code);
+
+// setTimeout((items) => {
+//   return (items = code);
+// });
+    
